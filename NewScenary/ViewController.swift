1633969af6908d8b7f2cff85120b409e4620523c
Takumi Kimura <shachibou@macbook-pro-5.local>
2018-02-23 12:11:52 -0500

    Initial Commit

---
+++ NewScenary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewScenary
+//
+//  Created by Takumi Kimura on 2018/02/23.
+//  Copyright © 2018年 com.takumi0kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
